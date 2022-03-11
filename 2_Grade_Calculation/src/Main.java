e9be19334b334c1df759ff985c2d4fdc948dc9a5
birdesen <87832209+birdesen@users.noreply.github.com>
2022-03-11 15:47:00 +0300

    Add files via upload

---
+++ 2_Grade_Calculation/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int math, physics, chemistry, turkish, history, music;
+
+        System.out.print("Enter your Math grade: ");
+        math = input.nextInt();
+
+        System.out.print("Enter your Physics grade: ");
+        physics = input.nextInt();
+
+        System.out.print("Enter your Chemistry grade: ");
+        chemistry = input.nextInt();
+
+        System.out.print("Enter your Turkish Language grade: ");
+        turkish = input.nextInt();
+
+        System.out.print("Enter your History grade: ");
+        history = input.nextInt();
+
+        System.out.print("Enter your Music grade: ");
+        music = input.nextInt();
+
+        int toplam = (math + physics + chemistry + turkish + history + music);
+        double sonuc = toplam / 6.0;
+        System.out.println("Your average grade: " + sonuc);
+
+
+
+    }
+}
